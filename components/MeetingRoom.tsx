cdbbf520c38e0ce0e08389a66335252717aedaa7
itzz-aryan-121 <122987294+itzz-aryan-121@users.noreply.github.com>
2024-07-10 00:35:52 +0530

    Update MeetingRoom.tsx

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,48 +1,114 @@
 'use client';
-
-import { tokenProvider } from '@/actions/stream.actions';
-import Loader from '@/components/Loader';
-import { useUser } from '@clerk/nextjs';
+import { useEffect, useState } from 'react';
 import {
-  StreamVideo,
-  StreamVideoClient,
+  CallControls,
+  CallParticipantsList,
+  CallStatsButton,
+  CallingState,
+  PaginatedGridLayout,
+  SpeakerLayout,
+  useCallStateHooks,
 } from '@stream-io/video-react-sdk';
-import { ReactNode, useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Users, LayoutList } from 'lucide-react';
 
-const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import Loader from './Loader';
+import EndCallButton from './EndCallButton';
+import { cn } from '@/lib/utils';
+import StreamVideoProvider from '@/providers/StreamClientProvider';
 
-export const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
-  const [videoClient, setVideoClient] = useState<StreamVideoClient>();
-  const { user, isLoaded } = useUser();
+type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 
-  useEffect(() => {
-    const initClient = async () => {
-      if (!isLoaded || !user) return;
-      if (!apiKey) throw new Error('Stream API Key is missing');
+const MeetingRoom = () => {
+  // useEffect(() => {
+  //   const audio = new Audio("/public/notification.mp3");
+  //   audio.play();
+  // }, []);
+  const searchParams = useSearchParams();
+  const isPersonalRoom = !!searchParams.get('personal');
+  const router = useRouter();
+  const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
+  const [showParticipants, setShowParticipants] = useState(false);
+  const { useCallCallingState } = useCallStateHooks();
 
-      const client = new StreamVideoClient({
-        apiKey,
-        user: {
-          id: user.id,
-          name: user?.username || user?.id,
-          image: user?.imageUrl,
-        },
-        tokenProvider,
-      });
+  // for more detail about types of CallingState see: https://getstream.io/video/docs/react/ui-cookbook/ringing-call/#incoming-call-panel
+  const callingState = useCallCallingState();
 
-      setVideoClient(client);
-    };
+  if (callingState !== CallingState.JOINED) return <Loader />;
 
-    initClient();
-  }, [user, isLoaded]);
-
-  if (!videoClient) return <Loader />;
+  const CallLayout = () => {
+    switch (layout) {
+      case 'grid':
+        return <PaginatedGridLayout />;
+      case 'speaker-right':
+        return <SpeakerLayout participantsBarPosition="left" />;
+      default:
+        return <SpeakerLayout participantsBarPosition="right" />;
+    }
+  };
 
   return (
-    <StreamVideo client={videoClient}>
-      {children}
-    </StreamVideo>
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className=" flex size-full max-w-[1000px] items-center">
+          <CallLayout />
+        </div>
+        <div
+          className={cn('h-[calc(100vh-86px)] hidden ml-2', {
+            'show-block': showParticipants,
+          })}
+        >
+          <CallParticipantsList onClose={() => setShowParticipants(false)} />
+        </div>
+      </div>
+      {/* video layout and call controls */}
+      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+        <CallControls onLeave={() => router.push(`/`)} />
+
+        <DropdownMenu>
+          <div className="flex items-center">
+            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+              <LayoutList size={20} className="text-white" />
+            </DropdownMenuTrigger>
+          </div>
+          <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
+            {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
+              <div key={index}>
+                <DropdownMenuItem
+                  onClick={() =>
+                    setLayout(item.toLowerCase() as CallLayoutType)
+                  }
+                >
+                  {item}
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className="border-dark-1" />
+              </div>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <CallStatsButton />
+        <button onClick={() => setShowParticipants((prev) => !prev)}>
+          <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+            <Users size={20} className="text-white" />
+          </div>
+        </button>
+        {!isPersonalRoom && <EndCallButton />}
+      </div>
+    </section>
   );
 };
 
-export default StreamVideoProvider;
+export default function MeetingRoomWithProvider() {
+  return (
+    <StreamVideoProvider>
+      <MeetingRoom />
+    </StreamVideoProvider>
+  );
+}
